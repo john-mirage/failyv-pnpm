2116692427b8b5c8a77fa63ab99198da3b0a3712
john-mirage <28307090+john-mirage@users.noreply.github.com>
2023-06-14 08:53:25 +0200

    feat: update plop setup

--- plopfile.js
+++ plopfile.js
@@ -1,4 +1,71 @@
 module.exports = function (plop) {
+  plop.setGenerator("application", {
+    description: "Adds a new application",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the application ?",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "apps/{{ name }}/vite.config.ts",
+        templateFile: "templates/application/vite.config.ts.hbs",
+      },
+      {
+        type: "add",
+        path: "apps/{{ name }}/tsconfig.json",
+        templateFile: "templates/application/tsconfig.json.hbs",
+      },
+      {
+        type: "add",
+        path: "apps/{{ name }}/tailwind.config.js",
+        templateFile: "templates/application/tailwind.config.js.hbs",
+      },
+      {
+        type: "add",
+        path: "apps/{{ name }}/postcss.config.js",
+        templateFile: "templates/application/postcss.config.js.hbs",
+      },
+      {
+        type: "add",
+        path: "apps/{{ name }}/package.json",
+        templateFile: "templates/application/package.json.hbs",
+      },
+      {
+        type: "add",
+        path: "apps/{{ name }}/index.html",
+        templateFile: "templates/application/index.html.hbs",
+      },
+      {
+        type: "add",
+        path: "apps/{{ name }}/.eslintrc.js",
+        templateFile: "templates/application/.eslintrc.js.hbs",
+      },
+      {
+        type: "add",
+        path: "apps/{{ name }}/src/index.tsx",
+        templateFile: "templates/application/src/index.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "apps/{{ name }}/src/index.css",
+        templateFile: "templates/application/src/index.css.hbs",
+      },
+      {
+        type: "add",
+        path: "apps/{{ name }}/src/components/app/app.tsx",
+        templateFile: "templates/application/src/components/app/app.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "apps/{{ name }}/src/components/app/index.tsx",
+        templateFile: "templates/application/src/components/app/index.tsx.hbs",
+      },
+    ],
+  });
   plop.setGenerator("component", {
     description: "Adds a new component",
     prompts: [
@@ -11,18 +78,13 @@ module.exports = function (plop) {
     actions: [
       {
         type: "add",
-        path: "packages/ui/src/components/faily-{{ name }}/faily-{{ name }}.ts",
-        templateFile: "templates/component/component.hbs",
+        path: "packages/ui/src/components/{{ name }}/{{ name }}.ts",
+        templateFile: "templates/component/component.tsx.hbs",
       },
       {
         type: "add",
-        path: "packages/ui/src/components/faily-{{ name }}/faily-{{ name }}.style.ts",
-        templateFile: "templates/component/component.style.hbs",
-      },
-      {
-        type: "add",
-        path: "packages/ui/src/components/faily-{{ name }}/index.ts",
-        templateFile: "templates/component/index.hbs",
+        path: "packages/ui/src/components/{{ name }}/index.ts",
+        templateFile: "templates/component/index.tsx.hbs",
       },
     ],
   });
--- templates/component/component.style.hbs
+++
@@ -1,9 +0,0 @@
-import { css } from "lit";
-
-const componentStyle = css`
-  :host {
-    color: blue;
-  }
-`;
-
-export default componentStyle;
